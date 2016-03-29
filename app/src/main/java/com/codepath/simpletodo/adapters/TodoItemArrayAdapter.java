8edbbffbc43a6917cf16569450f616b659d75287
pbabu <prabhu.babu@elementum.com>
2016-03-29 00:53:42 -0700

    added custom adapter and view holder to populate list view todo items

--- app/src/main/java/com/codepath/simpletodo/adapters/TodoItemArrayAdapter.java
+++ app/src/main/java/com/codepath/simpletodo/adapters/TodoItemArrayAdapter.java
@@ -16,6 +16,10 @@ import java.util.List;
  */
 public class TodoItemArrayAdapter extends ArrayAdapter<TodoItem> {
 
+    public static class ViewHolder {
+        TextView tvTodoItem;
+    }
+
     public TodoItemArrayAdapter(Context context, int resource, List objects) {
         super(context, resource, objects);
     }
@@ -25,12 +29,18 @@ public class TodoItemArrayAdapter extends ArrayAdapter<TodoItem> {
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
         final TodoItem todoItem = getItem(position);
+        ViewHolder viewHolder;
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+            TextView tvTodoItem = (TextView)convertView.findViewById(android.R.id.text1);
+            viewHolder = new ViewHolder();
+            viewHolder.tvTodoItem = tvTodoItem;
+            convertView.setTag(viewHolder);
+        }else {
+            viewHolder = (ViewHolder)convertView.getTag();
         }
-        TextView tvTodoItem = (TextView)convertView.findViewById(android.R.id.text1);
-        tvTodoItem.setText(todoItem.getName());
+        viewHolder.tvTodoItem.setText(todoItem.getName());
         return  convertView;
     }
     
