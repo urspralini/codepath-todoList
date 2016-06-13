3c6375668814aa5c80ea9f1520d276f45cc1d8c7
pbabu <prabhu.babu@elementum.com>
2016-06-13 08:22:41 -0700

    UI alignment changes

---
+++ app/src/main/java/com/codepath/simpletodo/activities/DetailItemActivity.java
@@ -0,0 +1,22 @@
+package com.codepath.simpletodo.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.codepath.simpletodo.R;
+
+public class DetailItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_item);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
