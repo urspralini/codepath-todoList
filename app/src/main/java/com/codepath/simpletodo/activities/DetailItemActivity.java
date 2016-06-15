379c49124f60877a0c478921efa8291e5d8ad6f5
pbabu <prabhu.babu@elementum.com>
2016-06-15 14:10:13 -0700

    customized spinner text and customized positions for priority and status

--- app/src/main/java/com/codepath/simpletodo/activities/DetailItemActivity.java
+++ app/src/main/java/com/codepath/simpletodo/activities/DetailItemActivity.java
@@ -1,15 +1,16 @@
 package com.codepath.simpletodo.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.codepath.simpletodo.R;
 
 public class DetailItemActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,18 @@ public class DetailItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail_item);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Spinner spinnerStatus = (Spinner)findViewById(R.id.spinnerStatus);
+        final ArrayAdapter<CharSequence> statusAdapter = ArrayAdapter.createFromResource(this,
+                R.array.status_arrays,
+                R.layout.my_spinner_textview);
+        statusAdapter.setDropDownViewResource(R.layout.my_spinner_textview);
+        spinnerStatus.setAdapter(statusAdapter);
+        Spinner spinnerPriority = (Spinner)findViewById(R.id.spinnerPriority);
+        final ArrayAdapter<CharSequence> priorityAdapter = ArrayAdapter.createFromResource(this,
+                R.array.priority_arrays,
+                R.layout.my_spinner_textview);
+        priorityAdapter.setDropDownViewResource(R.layout.my_spinner_textview);
+        spinnerPriority.setAdapter(priorityAdapter);
     }
 
 }
